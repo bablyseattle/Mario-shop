60804ae302e494ca1de943bf24dd486f77b2919d
Bably Kumari <bably@Bablys-MacBook-Pro.local>
2017-11-17 12:26:16 -0800

    add edit delete routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,9 +28,27 @@ class ProductsController < ApplicationController
 	    render :edit
 	  end
 
+	  def update
+	    @product = Product.new(product_params)
+	    if @product.update(product_params)
+	      redirect_to  products_path
+	    else
+	      render :edit
+	    end
+	  end
+
+	  def destroy
+	    @product = Product.find(params[:id])
+	    @product.destroy
+	    redirect_to  products_path
+	  end
+
+
 	private
 	  def product_params
 	    params.require(:product).permit(:name)
 	  end
+
+
 
 end
